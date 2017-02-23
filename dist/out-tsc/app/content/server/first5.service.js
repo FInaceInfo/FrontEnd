4724535c089869489dd79ab1ee1c0c61ac47bf1c
HUANG SIZHE <hsz1273327@gmail.com>
2017-02-23 23:39:46 +0800

    update
    update

--- dist/out-tsc/app/content/server/first5.service.js
+++ dist/out-tsc/app/content/server/first5.service.js
@@ -8,79 +8,24 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 import { Injectable } from '@angular/core';
-import { Headers, Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-var HeroService = (function () {
-    function HeroService(http) {
+var IndexWsService = (function () {
+    function IndexWsService(http) {
         this.http = http;
-        this.heroesUrl = 'app/heroes';
-        this.headers = new Headers({ 'Content-Type': 'application/json' });
+        this.IndexWsUrl = 'ws://';
     }
-    HeroService.prototype.handleError = function (error) {
-        var errMsg;
-        if (error instanceof Response) {
-            var body = error.json() || '';
-            var err = body.error || JSON.stringify(body);
-            errMsg = error.status + " - " + (error.statusText || '') + " " + err;
-        }
-        else {
-            errMsg = error.message ? error.message : error.toString();
-        }
-        console.error(errMsg);
-        return Promise.reject(errMsg);
-    };
-    HeroService.prototype.extractData = function (res) {
-        var body = res.json();
-        return body.data.map(function (data) { return new Hero(data.id, data.name, data.power, data.state); });
-    };
-    HeroService.prototype.getHeroes = function () {
+    IndexWsService.prototype.getIndex = function () {
         return this.http.get(this.heroesUrl)
             .toPromise()
             .then(function (response) { return response.json().data.map(function (data) { return new Hero(data.id, data.name, data.power, data.state); }); })
             .catch(this.handleError);
     };
-    HeroService.prototype.getHeroesSlowly = function () {
-        var _this = this;
-        return new Promise(function (resolve) {
-            return setTimeout(resolve, 2000);
-        })
-            .then(function () { return _this.getHeroes(); });
-    };
-    HeroService.prototype.getHero = function (id) {
-        return this.getHeroes()
-            .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
-    };
-    HeroService.prototype.update = function (hero) {
-        var url = this.heroesUrl + "/" + hero.id;
-        return this.http
-            .put(url, JSON.stringify(hero), { headers: this.headers })
-            .toPromise()
-            .then(function () { return hero; })
-            .catch(this.handleError);
-    };
-    HeroService.prototype.create = function (name) {
-        return this.http
-            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
-            .toPromise()
-            .then(function (res) { return res.json().data; })
-            .catch(this.handleError);
-    };
-    HeroService.prototype.delete = function (id) {
-        var url = this.heroesUrl + "/" + id;
-        return this.http.delete(url, { headers: this.headers })
-            .toPromise()
-            .then(function () { return null; })
-            .catch(this.handleError);
-    };
-    HeroService.prototype.toggleState = function (hero) {
-        hero.state === 'active' ? this.currentHeroes.splice(this.currentHeroes.length - 1, 1) : this.currentHeroes.push(hero);
-        hero.toggleState();
-    };
-    return HeroService;
+    return IndexWsService;
 }());
-HeroService = __decorate([
+IndexWsService = __decorate([
     Injectable(),
     __metadata("design:paramtypes", [Http])
-], HeroService);
-export { HeroService };
+], IndexWsService);
+export { IndexWsService };
 //# sourceMappingURL=../../../../../src/app/content/server/first5.service.js.map
